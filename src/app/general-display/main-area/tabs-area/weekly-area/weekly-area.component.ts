314c46d515d63fda9e5f918e32be20fe33e357f9
SarahEssaam <sarahessam29@hotmail.com>
2021-02-12 17:41:11 +0200

    Some style fixes in front page

--- src/app/general-display/main-area/tabs-area/weekly-area/weekly-area.component.ts
+++ src/app/general-display/main-area/tabs-area/weekly-area/weekly-area.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { WeatherData } from 'src/app/models/weather-data.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-weekly-area',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weekly-area.component.css']
 })
 export class WeeklyAreaComponent implements OnInit {
-
-  constructor() { }
+  @Input() weatherData: WeatherData;
+  param: string = 'avge'
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.data.weatherData.subscribe((newWeatherData)=>{
+      console.log("in weekly");
+      this.weatherData = newWeatherData;
+      console.log(this.weatherData);
+    });
+    
   }
 
 }
